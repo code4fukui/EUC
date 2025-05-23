a1774bde8f509db76b09d84aeafc5fee1db9f94f
Taisuke Fukuno <fukuno@jig.jp>
2025-05-24 08:38:04 +0900

    change to use local

--- EUC.example.js
+++ EUC.example.js
@@ -1,4 +1,4 @@
-import { EUC } from "https://code4fukui.github.io/EUC/EUC.js";
+import { EUC } from "./EUC.js";
 
 console.log(EUC.encode("あ"));
 console.log(EUC.decode(new Uint8Array([164, 162])));
